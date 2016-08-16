26b070a00c06d4e26a5d476d9dbaf3317bcd4ff2
NStephenson <nickstephenson90@gmail.com>
2016-08-15 21:24:24 -0400

    added PollController

---
+++ app/assets/javascripts/app/controllers/PollController.js
@@ -0,0 +1,11 @@
+app.controller('PollController', function PollController(poll, Auth){
+  var ctrl = this;
+
+  Auth.currentUser().then(function(user) { 
+    ctrl.currentUser = user; 
+  });
+
+  ctrl.poll = poll.data;
+
+
+});
